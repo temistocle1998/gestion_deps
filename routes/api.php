8f51ef364d2d527fdb892fcbf8acda1e1f93e19d
Lamine Beye <laminebeye007@gmail.com>
2021-09-28 16:32:00 +0000

    cors fixed

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 header('Access-Control-Allow-Origin:  *');
 header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
-header('Access-Control-Allow-Headers: Accept, Content-Type, X-Auth-Token, Origin, Authorization');
+header('Access-Control-Allow-Headers: Authorization');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
