6aa9713de987ff5f975321564adcb04370f5acab
Lamine Beye <laminebeye007@gmail.com>
2021-09-28 16:38:28 +0000

    cors fixed

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Headers: Authorization');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
