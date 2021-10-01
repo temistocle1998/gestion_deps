eebe5994d4621e0f3b75204d386c76a5f013ff9b
Lamine Beye <laminebeye007@gmail.com>
2021-10-01 18:03:25 +0000

    add feature

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\DepenseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 header("Cache-Control: no-cache, must-revalidate");
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header('Access-Control-Allow-Origin:  *');
 header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
