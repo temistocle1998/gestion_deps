4a4b4dc7b4c75fffe1fd4aaf446a127480916cf6
Lamine Beye <laminebeye007@gmail.com>
2021-10-06 11:58:25 +0000

    issue cors

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -15,7 +15,7 @@ class CORS
      */
     public function handle($request, Closure $next)
     {
-        header('Access-Control-Allow-Origin:  https://gestion-depense-angular.herokuapp.com');
+        header('Access-Control-Allow-Origin', '*');
         header('Access-Control-Allow-Methods', "GET,POST,PUT,PATCH,DELETE,OPTIONS");
         header('Access-Control-Allow-Headers: Accept,Content-type, Origin, Authorization, X-Requested-With');
         return $next($request);
