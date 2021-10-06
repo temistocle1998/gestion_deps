d51f9b14651e6705baa00f3642cd11c8291086ff
Lamine Beye <laminebeye007@gmail.com>
2021-10-06 14:32:22 +0000

    issue cors

--- app/TypeRevenu.php
+++ app/TypeRevenu.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class TypeRevenu extends Model
 {
+	private $guarded =[];
     public function revenu(): HasMany
     {
         return $this->hasMany(Revenu::class);
