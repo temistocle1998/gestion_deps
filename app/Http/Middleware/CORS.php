19afb6a8c75b733c9b7ce34afc1d7c7f8e8c83e3
Lamine Beye <laminebeye007@gmail.com>
2021-10-06 12:13:45 +0000

    issue cors

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -19,5 +19,7 @@ class CORS
         $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, DELETE');
         $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
         $response->header('Access-Control-Allow-Origin', '*');
+                return $response;
+
     }
 }
